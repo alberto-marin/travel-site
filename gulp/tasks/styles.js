254fa06ae7ecdf790fc9eeb851a35d65211c3925
Alberto Marin <albertomr84@gmail.com>
2017-03-22 12:43:29 +0000

    Add gulp error handling

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -10,5 +10,9 @@ gulp.task('styles', function() {
   // we put return because gulp.src is an asynchronus function
   return gulp.src('./app/assets/styles/styles.css')
     .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .on('error', function(errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit('end');
+    })
     .pipe(gulp.dest('./app/temp/styles'));
 });
