6f64274ce4459a79369d14bac5ad13facbe7fc98
Alberto Marin <albertomr84@gmail.com>
2017-03-28 14:44:10 +0100

    Add modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,13 +2,15 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
-
+import Modal from './modules/Modal'
 var mobileMenu = new MobileMenu();
 
 new RevealOnScroll($('.feature-item'), '85%'); // for feature items
 new RevealOnScroll($('.testimonial'), '60%'); // for testimonials
 
 var stickyHeader = new StickyHeader();
+
+var modal = new Modal();
 // // require is from node.js
 // var $ = require('jquery');
 // //var Person = require('./modules/Person');
